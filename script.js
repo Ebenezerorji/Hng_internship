298665a68edfd687c921ffd91df1155e74ced59f
Ebenezer orji <ebenezerorji7@gmail.com>
2023-09-09 18:58:49 +0100

    timer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     const currentDate = new Date();
    
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesdßay', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
     
     const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
@@ -10,14 +10,19 @@ document.addEventListener('DOMContentLoaded', function() {
     
 
 });
+
 function updateClock() {
-    const clockElement = document.getElementById('clock');
+    const clockElement = document.getElementById('currenttime');
     const currentDate = new Date();
-    const timeString = currentDate.toLocaleTimeString(); 
-    clockElement.textContent = timeString; // Update the content of the clock element
+    const hours = currentDate.getHours().toString().padStart(2, '0'); // Get hours in 24-hour format
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+    const milliseconds = currentDate.getMilliseconds().toString().padStart(3, '0');
+    
+    const timeString = `${hours}${minutes}${seconds}${milliseconds}`;
+    clockElement.textContent = timeString;
 }
 
-
+// Update the clock immediately and then every millisecond (1 millisecond)
 updateClock();
-setInterval(updateClock, 1000);
-
+setInterval(updateClock, 1);
