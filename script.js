2e07763a185822014872c0c6f24d971fffab9aa3
Ebenezer orji <142674487+Ebenezerorji@users.noreply.github.com>
2023-09-09 16:09:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function updateClock() {
     const clockElement = document.getElementById('clock');
-    const currentdate = new Date();
-    const timeString = currentdate.toLocaleTimeString() + '.' + currentdate.getMilliseconds().toString().padStart(3, '0'); 
-    clockElement.textContent = timeString; // Update the content of the clock element
+    const currentDate = new Date();
+    const hours = currentDate.getHours().toString().padStart(2, '0'); // Get hours in 24-hour format
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+    const milliseconds = currentDate.getMilliseconds().toString().padStart(3, '0');
+    
+    const timeString = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    clockElement.textContent = timeString;
 }
 
+// Update the clock immediately and then every millisecond (1 millisecond)
 updateClock();
-setInterval(updateClock, 100);
+setInterval(updateClock, 1);
+
 
